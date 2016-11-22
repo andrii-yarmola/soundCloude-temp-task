26b078bf7311795395b92bccd17f82cf41326cd8
Andrii Yarmola <andrii.yarmola@psd2html.com>
2016-11-22 18:50:41 +0200

    config

--- webpack.config.js
+++ webpack.config.js
@@ -6,49 +6,12 @@ module.exports = {
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
-  module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
-    ],
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    },
-    {
-      test: /\.scss$/,
-      loader: 'style!css!sass'
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist',
-    hot: true,
-    historyApiFallback: true
-  },
-  eslint: {
-    configFile: './.eslintrc'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"development"'
-      }
-    })
-  ]
+    contentBase: './dist'
+  }
 };
