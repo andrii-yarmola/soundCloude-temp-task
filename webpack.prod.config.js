3de8a94a9582ac3ec281cc3d48a325b75e673d69
Andrii Yarmola <andrii.yarmola@psd2html.com>
2016-11-22 17:28:17 +0200

    done

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -16,7 +16,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'react-hot!babel'
+      loader: 'babel-loader'
     },
     {
       test: /\.scss$/,
