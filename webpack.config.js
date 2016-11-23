6a47f88f2ca1fcaf4498bc87c9936f0a4c5d109b
andrii yarmola <andrii.yarmola@w3.co>
2016-11-23 18:37:24 +0200

    1st component and test command

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,23 @@ module.exports = {
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }
 };
